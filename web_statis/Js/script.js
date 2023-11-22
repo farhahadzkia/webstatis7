86d38c7fe71360623287c715cbce640e7a29245f
farhahadzkia <144687262+farhahadzkia@users.noreply.github.com>
2023-11-23 00:14:42 +0700

    script.js

--- web_statis/Js/script.js
+++ web_statis/Js/script.js
@@ -5,6 +5,16 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// toggle class active untuk seacrh from
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 // Klik di luar elemen
 const hm = document.querySelector("#hamburger-menu");
 
